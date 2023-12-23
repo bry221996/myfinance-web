473668dcbf67f9fb31b51c63b067e89f0b478034
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2023-12-23 23:14:01 +0800

    Add icon

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -1,5 +1,6 @@
 import React, { ComponentProps } from 'react'
 import { ProfileType } from '@/types/Profile'
+import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
 
 interface ProfileCardProps extends ComponentProps<'li'> {
   profile: ProfileType
@@ -11,13 +12,22 @@ const ProfileCard = ({
   ...props
 }: ProfileCardProps) => {
   return (
-    <li className={`bg-white rounded-2xl p-4 ${className}`} {...props}>
-      <h5 className="text-lg font-semibold leading-[120%] text-[#002034] tracking-[-1px]">
-        {profile.name}
-      </h5>
-      <p className="text-[#4D6371] font-medium leading-[120%] mt-2">
-        {profile.description}
-      </p>
+    <li
+      className={`bg-white rounded-2xl p-4 flex justify-between ${className}`}
+      {...props}>
+      <div>
+        <h5 className="text-lg font-semibold leading-[120%] text-[#002034] tracking-[-1px]">
+          {profile.name}
+        </h5>
+        <p className="text-[#4D6371] font-medium leading-[120%] mt-2">
+          {profile.description}
+        </p>
+      </div>
+
+      <div className="flex space-x-2 text-[#4D6371]">
+        <PencilSquareIcon className="w-5 h-5" />
+        <TrashIcon className="w-5 h-5" />
+      </div>
     </li>
   )
 }
